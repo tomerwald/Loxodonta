9700faa001a53044797768de5f608c06ddb16b60
Tomer Waldman <defsaf9@gmail.com>
2020-12-06 20:28:39 +0200

    Added CDP

--- loxodonta/analyzer/data_link/ref.py
+++ loxodonta/analyzer/data_link/ref.py
@@ -1,9 +1,17 @@
 class Entities:
     MAC = "MAC"
     Manufacturer = "Manufacturer"
+    PortID = "PortID"
+    Vlan = "Vlan"
+    DeviceID = "DeviceID"
+    SoftwareVersion = "SoftwareVersion"
+    Platform = "Platform"
+    Role = "Role"
 
 
 class Connections:
     EthTraffic = "EthernetTraffic"
     ManufResolve = "ManufacturerResolve"
     ArpResolve = "ArpResolve"
+    PhysicalConnection = "PhysicalConnection"
+    CDPAnnouncement = "CDPAnnouncement"
