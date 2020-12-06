2d75925c073f0dc085c1fc036c0be5adfdba5bf7
Tomer Waldman <defsaf9@gmail.com>
2020-12-06 21:44:53 +0200

    Added vlan

--- loxodonta/analyzer/data_link/__init__.py
+++ loxodonta/analyzer/data_link/__init__.py
@@ -2,5 +2,6 @@ from loxodonta.analyzer.data_link.arp import ARP
 from loxodonta.analyzer.data_link.ethernet import Ethernet
 from loxodonta.analyzer.data_link.ref import Connections, Entities
 from loxodonta.analyzer.data_link.cdp import CDP
+from loxodonta.analyzer.data_link.vlan import VLAN
 
 all = [ARP, Ethernet, CDP]
--- loxodonta/analyzer/network/ref.py
+++ loxodonta/analyzer/network/ref.py
@@ -1,6 +1,8 @@
 class Entities:
     IP = "IP"
     IPv6 = "IPv6"
+    Subnet = "Subnet"
+    VPN = "VPN"
 
 
 class Connections:
